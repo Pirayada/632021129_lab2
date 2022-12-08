581a9fbda149965983fd92a5b6922420c48750c1
Pirayada Maschay <632021129@tsu.ac.th>
2022-12-08 17:14:50 +0700

    First Commit

---
+++ lib/firstpage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pirayada Maschay"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(children: [
+        Image.asset(
+          "assets/image/A.jpg",
+        )
+      ]),
+    );
+  }
+
+  Row buildTitle() {
+    return Row(
+      children: [
+        Column(
+          children: const [Text('ooooo'), Text('kkkkk')],
+        )
+      ],
+    );
+  }
+}
